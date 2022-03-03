20050d5b6bf78200ba12096823a636662c462493
Kaze Shindo <kaze.bknb@outlook.jp>
2022-03-03 12:01:02 +0900

    fix: change app.port to os.environ.get

--- src/main.py
+++ src/main.py
@@ -59,4 +59,5 @@ def handle_message(event):
 
 # python main.py　で動作
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
