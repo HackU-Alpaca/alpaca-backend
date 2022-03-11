5eb32309f3e4bda29b67cb3ac2c7e5b192de4f04
Satoshi Nakamura <s3104@ymail.ne.jp>
2022-03-11 21:18:28 +0900

    add validation rule & add counter

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from flask_bootstrap import Bootstrap
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import (FlexSendMessage, MessageEvent, StickerSendMessage,
                             TextMessage, TextSendMessage)
+
 from message_form import MessageForm
 from models.message import *
 from models.tag import tag_collection
@@ -199,5 +200,5 @@ def get_cheer_message_by_tag():
 
 # python main.py　で動作
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5002))
     app.run(host='0.0.0.0', port=port, debug=True)
